aaf69a662d7f28c5273ad9cf7a6cf580340e2b8f
Bruno Bachmann <brusinski.bochmann@web.de>
2021-05-09 12:43:15 +0200

    Aufgabe  7

---
+++ Steckbrief/L07/Aufgabe07.js
@@ -0,0 +1,42 @@
+var sample1 = document.querySelector('.sample1');
+var sample2 = document.querySelector('.sample2');
+var sample3 = document.querySelector('.sample3');
+var sample4 = document.querySelector('.sample4');
+var sample5 = document.querySelector('.sample5');
+var sample6 = document.querySelector('.sample6');
+var sample7 = document.querySelector('.sample7');
+var sample8 = document.querySelector('.sample8');
+var sample9 = document.querySelector('.sample9');
+var playButton = document.querySelector('.playButton');
+var sounds = [];
+sounds[0] = new Audio("Samples/A.mp3");
+sounds[1] = new Audio("Samples/C.mp3");
+sounds[2] = new Audio("Samples/F.mp3");
+sounds[3] = new Audio("Samples/G.mp3");
+sounds[4] = new Audio("Samples/hihat.mp3");
+sounds[5] = new Audio("Samples/kick.mp3");
+sounds[6] = new Audio("Samples/laugh-1.mp3");
+sounds[7] = new Audio("Samples/laugh-2.mp3");
+sounds[8] = new Audio("Samples/snare.mp3");
+var beatfolge = [];
+beatfolge[0] = sounds[3];
+beatfolge[1] = sounds[1];
+beatfolge[2] = sounds[0];
+beatfolge[3] = sounds[2];
+sample1.addEventListener("click", function () { sounds[0].play(); });
+sample2.addEventListener("click", function () { sounds[1].play(); });
+sample3.addEventListener("click", function () { sounds[2].play(); });
+sample4.addEventListener("click", function () { sounds[3].play(); });
+sample5.addEventListener("click", function () { sounds[4].play(); });
+sample6.addEventListener("click", function () { sounds[5].play(); });
+sample7.addEventListener("click", function () { sounds[6].play(); });
+sample8.addEventListener("click", function () { sounds[7].play(); });
+sample9.addEventListener("click", function () { sounds[8].play(); });
+playButton.addEventListener("click", beat);
+function beat() {
+    setTimeout(function () { beatfolge[0].play(); }, 500);
+    setTimeout(function () { beatfolge[1].play(); }, 1000);
+    setTimeout(function () { beatfolge[2].play(); }, 1500);
+    setTimeout(function () { beatfolge[3].play(); }, 2000);                             
+}
+//# sourceMappingURL=Aufgabe07.js.map
