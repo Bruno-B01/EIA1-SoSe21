824d528fbf8decf676113b596ac235b26e7c9fd5
Bruno Bachmann <brusinski.bochmann@web.de>
2021-05-22 15:52:13 +0200

    aufgabe9

---
+++ Steckbrief/L09/Aufgabe09.ts
@@ -0,0 +1,49 @@
+let taskCounter: number = 0;
+let userinput: HTMLInputElement = document.querySelector("#addTask") as HTMLInputElement;
+
+userinput.addEventListener("keypress", function (keyboardEvent: KeyboardEvent): void {
+
+if (keyboardEvent.key == "Enter") {
+
+    let inputTask: Text = new Text(userinput.value);
+    taskCounter = taskCounter + 1;
+    document.querySelector("h3").innerHTML = taskCounter + " in total";
+  
+    let newTask: HTMLDivElement = document.createElement("div");
+    newTask.className = "task01";
+    document.querySelector(".additionalTasks").appendChild(newTask);
+
+    const taskOpen: HTMLElement = document.createElement("i");
+    taskOpen.classList.add("far", "fa-circle");
+
+    const taskDone: HTMLElement = document.createElement("i");
+    taskDone.classList.add("far", "fa-check-circle", "isHidden");
+
+    const deleted: HTMLElement = document.createElement("i");
+    deleted.classList.add("far", "fa-trash-alt");
+
+    newTask.appendChild(inputTask);
+    newTask.appendChild(taskOpen);
+    newTask.appendChild(taskDone);
+    newTask.appendChild(deleted);
+  
+    function changeClasses(firstHTMLElement: HTMLElement, secondHTMLElement: HTMLElement): void {
+        firstHTMLElement.classList.add("isHidden");
+        secondHTMLElement.classList.remove("isHidden");
+    }
+
+    taskOpen.addEventListener("click", function (): void {
+        changeClasses(taskOpen, taskDone);
+    });
+
+    taskDone.addEventListener("click", function (): void {
+        changeClasses(taskDone, taskOpen);
+    });
+
+    deleted.addEventListener("click", function (): void {
+        newTask.classList.add("isHidden");
+        taskCounter--;
+        document.querySelector("h3").innerHTML = taskCounter + " in total";
+    });
+  }
+});
